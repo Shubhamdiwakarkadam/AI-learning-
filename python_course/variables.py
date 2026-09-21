02c28fa923608b3fc0e8e9851e178cfdf67be7ea
SHUBHAM DIWAKAR KADAM <shubhamdiwakarkadam@gmail.com>
2026-09-21 12:50:09 +0530

    Create variables.py

---
+++ python_course/variables.py
@@ -0,0 +1,23 @@
+#assigning a value to a variable
+
+name = "shubham"
+print(name)
+
+#dynamic typing
+name = 2.5
+print(name)
+
+name = True
+print(name)
+
+
+#speacial syntax for multiple assignment
+
+a,b,c =1,2,3
+print(a)
+
+
+a=5;b=43;c=20323
+print(a)
+print(b)
+print(c)
