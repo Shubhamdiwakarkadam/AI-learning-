ae863f5defa0cd5819bd27789b214f3a59b97f67
SHUBHAM DIWAKAR KADAM <shubhamdiwakarkadam@gmail.com>
2026-09-21 13:30:11 +0530

    practice

---
+++ practice-questions.py
@@ -0,0 +1,257 @@
+#Practice questions
+#Beginner level
+##"Hello, world!"
+
+#Q1:Write a Python program to print: "Hello, world!"
+
+print("hello wolrd")
+
+
+
+
+
+#------------------------------------
+k= "----------------------------------------"
+print(k,)
+#------------------------------------
+
+
+
+
+
+
+#Q2:Create a variable named name and assign your name to it. Then print it.
+name="Shubham"
+print(name)
+
+
+
+
+
+
+k= "----------------------------------------"
+print(k,)
+#------------------------------------
+
+
+
+
+
+
+
+#Q3   Assign x = 10 and y = 5. Print the sum of both.
+x= 10
+y=5
+print(x+y)
+
+
+
+
+
+
+
+#------------------------------------
+k= "----------------------------------------"
+print(k)
+
+
+
+
+
+
+
+#Write code to print the values of:
+
+#a = 7
+#b = 3.5
+#c = "Python"
+
+
+a=7
+b=3.5
+c="python"
+
+print(a,b,c)
+
+
+
+
+
+
+
+
+#------------------------------------
+k= "----------------------------------------"
+print(k)
+#------------------------------------
+
+
+
+
+
+
+
+
+
+#Q5   What is the output of this? print(2 + 3)
+
+print(2 + 3)
+
+
+
+
+
+
+
+#------------------------------------
+k= "----------------------------------------"
+print(k)
+#------------------------------------
+
+
+
+
+
+
+
+#Q6   Write a program that stores your age in a variable and prints it.
+
+my_age= 26
+print(my_age)
+
+
+
+
+
+
+
+
+#------------------------------------
+k= "----------------------------------------"
+print(k)
+#------------------------------------
+
+
+
+
+
+
+
+
+#Q7   Create three variables a, b, and c in one line and print all of them.
+
+a,b,c=23,12,1999
+print(a,b,c)
+#------------------------------------
+k= "----------------------------------------"
+print(k)
+#------------------------------------
+
+
+
+
+
+
+
+#Q8    Print a boolean value like: print(True)
+print(False)
+
+
+#------------------------------------
+k= "----------------------------------------"
+print(k,)
+#------------------------------------
+
+
+
+
+
+
+#Q9 Data types practice
+#Write code to print:
+#an integer
+# a float
+#a string
+#a boolean
+
+
+
+a= 231299
+b=3.14
+c="elon msuk"
+d="False"
+
+print(a,b,c,d)
+print(a)
+print(b)
+print(c)
+print(d)
+
+
+
+
+
+
+#------------------------------------
+k= "----------------------------------------"
+print(k,)
+#------------------------------------
+
+
+
+
+
+#Q10 Create a list: numbers = [1, 2, 3, 4, 5] and print it.
+
+
+number=([1,2,3,4,5])
+print(number)
+
+
+
+
+#------------------------------------
+k= "----------------------------------------"
+print(k,)
+#------------------------------------
+
+
+
+
+#Q 11 .   Create a tuple with 3 values and print it.
+
+
+numbers= ((1,2,3))
+print(numbers)
+
+
+
+
+#------------------------------------
+k= "----------------------------------------"
+print(k,)
+#------------------------------------
+
+
+
+
+#Q12. Create a set with 4 numbers and print it.
+
+numberss= ({1,2,3,4,5})
+print(numberss)
+
+
+#------------------------------------
+k= "----------------------------------------"
+print(k,)
+#------------------------------------
+
+
+
+
+#Q 13 Create a dictionary :
+person = {"name": "Alice", "age": 20}
+print(person)
+
+
+
