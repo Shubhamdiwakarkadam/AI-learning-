b65ca84e8a344b0b814fbc9be0956bae90ab6438
SHUBHAM DIWAKAR KADAM <shubhamdiwakarkadam@gmail.com>
2026-09-21 12:31:36 +0530

    Create data-types.py

---
+++ data-types.py
@@ -0,0 +1,32 @@
+#basic data types
+
+#intiger
+print(1e308)
+
+#float
+print(10.5)
+
+#string
+print("hello world")
+
+#boolean
+print(True)
+print(False)    
+
+#complex
+print(2+3j)
+
+#--------------------------------------------------
+
+#container types
+#list
+print([1,2,3,4,5])
+
+#tuple
+print((1,2,3,4,5))
+
+#sets
+print({1,2,3,4,5})
+
+#dictionary
+print({"name":"shubham","age":22,"gender":"male"})
